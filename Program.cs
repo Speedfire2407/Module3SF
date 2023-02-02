74e4463fec12014e9ce3d6df5392bcb6dce22653
Speedfire2407 <Faunsatyr@yandex.ru>
2023-02-02 23:11:55 +0500

    add data files in directory

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FirstTask
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName = "Alexander";
+            Console.WriteLine(myName);
+            Console.ReadKey();
+        }
+    }
+}
