e2bf51dcb8e8fa62b15ab127521e36b2bf01ab25
Speedfire2407 <Faunsatyr@yandex.ru>
2023-02-03 22:45:23 +0500

    made 2nd task

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 ﻿namespace FirstTask
 {
-    internal class Program
+    internal class Program2
     {
         static void Main(string[] args)
         {
             string myName = "Alexander";
-            Console.WriteLine(myName);
+            int age = 29;
+            bool haveYouPet = false;
+            float shoeSize = 42;
+            Console.WriteLine("My name is " + myName + "\nMy age is " + age + "\nDo I have a pet? " + haveYouPet + "\nMy shoe size is " + shoeSize);
             Console.ReadKey();
         }
     }
