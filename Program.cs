7bad0e49311f4b9e347db43aecf91ec57ca8452a
Speedfire2407 <Faunsatyr@yandex.ru>
2023-02-03 23:18:31 +0500

    add some enums

--- Program.cs
+++ Program.cs
@@ -2,8 +2,25 @@
 {
     internal class Program2
     {
+        enum DaysOfWeek: byte
+        {
+            Monday = 1,
+            Tuesday,
+            Wednesday,
+            Thursday,
+            Friday,
+            Saturday,
+            Sunday
+        }
+        enum Semophone: short
+        {
+            Red = 100,
+            Yellow = 200,
+            Green = 300
+        }
         static void Main(string[] args)
         {
+            
             string myName = "Alexander";
             int age = 29;
             bool haveYouPet = false;
