7e7d06e8505d99c859c52f6a0261fe20b6d20e27
Speedfire2407 <Faunsatyr@yandex.ru>
2023-02-04 13:16:21 +0500

    some fix

--- TellAboutYourself.cs
+++ TellAboutYourself.cs
@@ -1,7 +1,10 @@
 ﻿namespace FirstTask
 {
-    internal class AboutMe
+    internal class TellAboutYourself
     {
+        /// <summary>
+        /// Перечисления дней недели
+        /// </summary>
         enum DaysOfWeek: byte
         {
             Monday = 1,
@@ -18,6 +21,10 @@
             Yellow = 200,
             Green = 300
         }
+        /// <summary>
+        /// Программа задает вопросы о пользователе
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.Write("Enter you're name: ");
@@ -31,7 +38,7 @@
             Console.WriteLine("What is your shoe size? ");
             byte shoeSize = checked((byte)int.Parse(Console.ReadLine()));
             Console.WriteLine("You're {0} cm tall and you have shoes size {1}", yourTall, shoeSize);
-            Console.Write("What is your favorite day of week?");
+            Console.Write("What is your favorite day of week? ");
             var day = (DayOfWeek)int.Parse(Console.ReadLine());
             Console.WriteLine("Your favorite day is {0}", day);
             Console.ReadKey();
